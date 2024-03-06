d6daf9ec64edf6d19e364d345cc9233051b54582
Jean-Marie CHATEAUX <jmc@jimbook.home>
2024-03-06 19:37:55 +0100

    projects cards + dark mode

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,29 @@
 import { NavLink } from "react-router-dom";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
 
 export default function ProjectCard({ name, cover, description, stack, live, code }) {
     return (
         <div className="project-card">
-            <h3 className="project-card-name">{name}</h3>
             <img src={cover} alt={name} className="project-card-cover" />
-            <p>{description}</p>
-            <ul>
-                {stack.map(c =>
-                    <li key={c}>{c}</li>
-                )}
-            </ul>
-            <a href={live} target="_blank">Version Live</a> <br />
-            <a href={code} target="_blank">Code</a>
+            <div className="project-card-right-container">
+                <div className="title-countainer">
+                    <h3 className="project-card-name">{name}</h3>
+                </div>
+                <div className="links-countainer">
+                    <a href={live} target="_blank" title="Visiter le site"><FaExternalLinkAlt /></a>
+                    <a href={code} target="_blank" title="Voir le code"><FaCode /></a>
+                </div>
+            </div>
+            <div className="project-card-bottom-container">
+                <p>{description}</p>
+                <ul>
+                    {stack.map(c =>
+                        <li key={c}>{c}/</li>
+                    )}
+                </ul>
+            </div>
+
         </div>
     )
 }
